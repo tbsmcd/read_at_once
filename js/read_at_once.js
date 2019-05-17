1122d3b697a258c63dea192c8eaed6a836745076
tbsmcd <tbsmcd@gmail.com>
2019-05-17 10:19:06 +0900

    Make check box unnecessary

--- js/read_at_once.js
+++ js/read_at_once.js
@@ -1,36 +1,35 @@
 rcmail.addEventListener('init', function(ev){
     // add read,unread buttons
     $('#messagelistfooter').prepend('<div id="rao-btns-wrap"><button class="rao-btn" id="rao-read-btn">' + rcmail.gettext('read', 'read_at_once') + '</button><button class="rao-btn" id="rao-unread-btn">' + rcmail.gettext('unread', 'read_at_once') + '</button></div>');
-    $('#messagelistfooter').prepend('<div id="rao-checkall-wrap"><label for="rao-checkall"><input type="checkbox" id="rao-checkall">' + rcmail.gettext('check_all', 'read_at_once') + '</label></div>');
-    // add check all box
-    $('#rao-checkall').on('change', function(){
-        $('input[class="rao-checkbox"]').prop('checked', this.checked);
+    $('#messagelistfooter').prepend('<div id="rao-checkall-wrap"><a href="#" id="rao-checkall">' + rcmail.gettext('check_all', 'read_at_once') + '</a> <a href="#" id="rao-uncheckall">' + rcmail.gettext('uncheck_all', 'read_at_once') + '</a></div>');
+    $('#rao-checkall').on('click', function(){
+        $('.message').addClass('selected');
+    });
+    $('#rao-uncheckall').on('click', function(){
+        $('.message').removeClass('selected');
     });
 
     $('.rao-btn').click(function(){
-        ckd = $('[class=rao-checkbox]:checked').map(function(){
-            return $(this).attr('name');
-        }).get();
         if ($(this).attr('id') == 'rao-read-btn') {
             mark = 'read';
         } else {
             mark = 'unread';
         }
-        ckd.forEach(function(uid){
-            rcmail.mark_message(mark, uid);
+        $('.message.selected').each(function(){
+            if(r = $(this).prop('className').match(/rao-selector-([0-9]+)/)){
+                rcmail.mark_message(mark, r[1]);
+            }
         });
     });
 });
 
 rcmail.addEventListener('listupdate', function(ev){
-    // add checkbox
-    $('.rao-checkbox').remove();
     rows = rcmail.message_list.rows;
     ids = {}
     Object.keys(rows).forEach(function(key){
         ids[rows[key]['id']] = rows[key]['uid']; 
     })
     $('#messagelist tbody tr').each(function(){
-        $(this).children('.subject').prepend('<input type="checkbox" name="' + ids[$(this).attr('id')] + '" class="rao-checkbox">');
+        $(this).addClass('rao-selector-' + ids[$(this).attr('id')]);
     })
 });
